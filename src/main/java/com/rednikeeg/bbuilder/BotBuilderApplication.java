1592484d65ba48063fc4f85d8f5b6715d2565745
rednikeeg <maks.schoolforvv@gmail.com>
2022-09-04 20:03:59 +0300

    Initialized project

---
+++ src/main/java/com/rednikeeg/bbuilder/BotBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.rednikeeg.bbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotBuilderApplication.class, args);
+    }
+
+}
